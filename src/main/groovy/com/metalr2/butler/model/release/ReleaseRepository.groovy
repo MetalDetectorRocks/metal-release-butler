7bec6d181fc193f7fc48e4e201936ea00d3c4c05
Daniel Wagner <wagner.daniel87@gmail.com>
2019-10-23 18:45:15 +0200

    delete any record in sql database that has a release date today or later before inserting new records

--- src/main/groovy/com/metalr2/butler/model/release/ReleaseRepository.groovy
+++ src/main/groovy/com/metalr2/butler/model/release/ReleaseRepository.groovy
@@ -2,6 +2,10 @@ package com.metalr2.butler.model.release
 
 import org.springframework.data.jpa.repository.JpaRepository
 
+import java.time.OffsetDateTime
+
 interface ReleaseRepository extends JpaRepository<ReleaseEntity, Long> {
+
+  int deleteByReleaseDateIsAfter(OffsetDateTime offsetDateTime)
 
 }
--- src/main/groovy/com/metalr2/butler/service/ReleaseServiceImpl.groovy
+++ src/main/groovy/com/metalr2/butler/service/ReleaseServiceImpl.groovy
@@ -3,12 +3,19 @@ package com.metalr2.butler.service
 import com.metalr2.butler.model.release.ReleaseEntity
 import com.metalr2.butler.model.release.ReleaseRepository
 import com.metalr2.butler.service.converter.Converter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+import java.time.LocalDate
+import java.time.ZoneId
 
 @Service
 class ReleaseServiceImpl implements ReleaseService {
 
+  final Logger log = LoggerFactory.getLogger(ReleaseServiceImpl)
   final ReleaseRepository releaseRepository
   final Converter<String[], List<ReleaseEntity>> converter
 
@@ -19,6 +26,7 @@ class ReleaseServiceImpl implements ReleaseService {
   }
 
   @Override
+  @Transactional
   void saveAll(List<String[]> upcomingReleasesRawData) {
     List<ReleaseEntity> releaseEntities = []
     upcomingReleasesRawData.each { releaseEntities.addAll(converter.convert(it)) }
@@ -27,6 +35,11 @@ class ReleaseServiceImpl implements ReleaseService {
     releaseEntities.unique { release1, release2 ->
       release1.artist <=> release2.artist ?: release1.albumTitle <=> release2.albumTitle ?: release1.releaseDate <=> release2.releaseDate
     }
+
+    // remove any record that has a release date today or later
+    def yesterday = LocalDate.now().minusDays(1).atStartOfDay(ZoneId.of("UTC")).toOffsetDateTime()
+    int affectedRows = releaseRepository.deleteByReleaseDateIsAfter(yesterday)
+    log.info("{} records were deleted", affectedRows)
 
     releaseRepository.saveAll(releaseEntities)
   }
