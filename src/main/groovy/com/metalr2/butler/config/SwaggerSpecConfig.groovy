c2ebb256f5aa5ae7be751374d832243d071c41e0
Daniel Wagner <wagner.daniel87@gmail.com>
2019-11-24 17:18:57 +0100

    add swagger configuration

---
+++ src/main/groovy/com/metalr2/butler/config/SwaggerSpecConfig.groovy
@@ -0,0 +1,28 @@
+package com.metalr2.butler.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider
+import springfox.documentation.swagger.web.SwaggerResource
+import springfox.documentation.swagger.web.SwaggerResourcesProvider
+
+@Configuration
+class SwaggerSpecConfig {
+
+  @Primary
+  @Bean
+  SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider) {
+    return {
+      SwaggerResource wsResource = new SwaggerResource()
+      wsResource.name = 'Metal Release Butler REST API Doc'
+      wsResource.swaggerVersion = '2.0'
+      wsResource.location = '/swagger.yml' // need to be a url
+
+      List<SwaggerResource> resources = new ArrayList<>(defaultResourcesProvider.get())
+      resources.add(wsResource)
+      return resources
+    }
+  }
+
+}
