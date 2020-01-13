e862aea61f4c89de9abc0aa016b0c78edc6c87a5
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-01-13 22:07:20 +0100

    Test added for ReleaseRepository (without pagination)

---
+++ src/test/groovy/com/metalr2/butler/model/release/ReleaseRepositoryIT.groovy
@@ -0,0 +1,93 @@
+package com.metalr2.butler.model.release
+
+import org.junit.jupiter.api.Tag
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.TestPropertySource
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.LocalDate
+
+@Tag("integration-test")
+@TestPropertySource(locations = "classpath:application-test.properties")
+@DataJpaTest
+class ReleaseRepositoryIT extends Specification {
+
+  @Autowired
+  ReleaseRepository underTest
+
+  static def release1 = new ReleaseEntity(artist: "A1", albumTitle: "T1", releaseDate: LocalDate.of(2020, 1, 1), state: ReleaseEntityRecordState.OK)
+  static def release2 = new ReleaseEntity(artist: "A2", albumTitle: "T2", releaseDate: LocalDate.of(2020, 2, 1), state: ReleaseEntityRecordState.OK)
+  static def release3 = new ReleaseEntity(artist: "A3", albumTitle: "T3", releaseDate: LocalDate.of(2020, 3, 1), state: ReleaseEntityRecordState.OK)
+
+  void setup() {
+    def releases = [release1, release2, release3]
+    underTest.saveAll(releases)
+  }
+
+  void cleanup() {
+    underTest.deleteAll()
+  }
+
+  @Unroll
+  def "Find all after #date"() {
+    when:
+    List<ReleaseEntity> results = underTest.findAllByReleaseDateAfter(date)
+
+    then:
+    results.containsAll(expectedReleases)
+
+    where:
+    date                       | expectedReleases
+    LocalDate.of(2019, 12, 31) | [release1, release2, release3]
+    LocalDate.of(2020, 1, 1)   | [release2, release3]
+    LocalDate.of(2020, 2, 28)  | [release3]
+    LocalDate.of(2020, 3, 31)  | []
+  }
+
+  @Unroll
+  def "Find all between #from and #to"() {
+    when:
+    List<ReleaseEntity> results = underTest.findAllByReleaseDateBetween(from, to)
+
+    then:
+    results.containsAll(expectedReleases)
+
+    where:
+    from                       | to                       | expectedReleases
+    LocalDate.of(2019, 12, 31) | LocalDate.of(2020, 1, 1) | [release1]
+    LocalDate.of(2020, 1, 1)   | LocalDate.of(2020, 2, 1) | [release1, release2]
+    LocalDate.of(2020, 2, 1)   | LocalDate.of(2020, 1, 1) | []
+  }
+
+  @Unroll
+  def "Find all after #date and artist in #artists"() {
+    when:
+    List<ReleaseEntity> results = underTest.findAllByReleaseDateAfterAndArtistIn(date, artists)
+
+    then:
+    results.containsAll(expectedReleases)
+
+    where:
+    date                       | artists            | expectedReleases
+    LocalDate.of(2019, 12, 31) | ["A1", "A2", "A3"] | [release1, release2, release3]
+    LocalDate.of(2019, 12, 31) | ["A1", "A2"]       | [release1, release2]
+    LocalDate.of(2019, 12, 31) | []                 | []
+  }
+
+  @Unroll
+  def "Find all between #from and #to and artist in #artists"() {
+    when:
+    List<ReleaseEntity> results = underTest.findAllByReleaseDateBetween(from, to)
+
+    then:
+    results.containsAll(expectedReleases)
+
+    where:
+    from                       | to                       | artists            | expectedReleases
+    LocalDate.of(2020, 1, 1)   | LocalDate.of(2020, 3, 1) | ["A1", "A2", "A3"] | [release1, release2, release3]
+    LocalDate.of(2019, 12, 31) | LocalDate.of(2020, 1, 1) | ["A1"]             | [release1]
+    LocalDate.of(2020, 1, 1)   | LocalDate.of(2020, 2, 1) | ["A1"]             | [release1]
+  }
+}
