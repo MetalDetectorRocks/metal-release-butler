28fad26c33612dc9d211daf72a71a9416b0405d7
hedgehog1833 <32484683+hedgehog1833@users.noreply.github.com>
2020-03-14 17:16:20 +0100

    Dependency updates (#7)
    https://trello.com/c/HSkvvGJn

--- src/main/groovy/rocks/metaldetector/butler/web/rest/ReleasesRestController.groovy
+++ src/main/groovy/rocks/metaldetector/butler/web/rest/ReleasesRestController.groovy
@@ -1,12 +1,6 @@
 package rocks.metaldetector.butler.web.rest
 
 import org.springframework.beans.factory.annotation.Autowired
-import rocks.metaldetector.butler.model.TimeRange
-import rocks.metaldetector.butler.service.ReleaseService
-import rocks.metaldetector.butler.web.dto.ReleaseImportResponse
-import rocks.metaldetector.butler.web.dto.ReleasesRequest
-import rocks.metaldetector.butler.web.dto.ReleasesRequestPaginated
-import rocks.metaldetector.butler.web.dto.ReleasesResponse
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,10 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import rocks.metaldetector.butler.model.TimeRange
+import rocks.metaldetector.butler.service.ReleaseService
+import rocks.metaldetector.butler.web.dto.ReleaseImportResponse
+import rocks.metaldetector.butler.web.dto.ReleasesRequest
+import rocks.metaldetector.butler.web.dto.ReleasesRequestPaginated
+import rocks.metaldetector.butler.web.dto.ReleasesResponse
 
 import javax.validation.Valid
 
-import static rocks.metaldetector.butler.config.Endpoints.*
+import static rocks.metaldetector.butler.config.Endpoints.RELEASES
+import static rocks.metaldetector.butler.config.Endpoints.UNPAGINATED
 
 @RestController
 @RequestMapping(RELEASES)
