6dd555840ecc9da449ffccdd51c8c0ce6c4950f2
Daniel Wagner <wagner.daniel87@gmail.com>
2020-03-16 14:09:09 +0100

    Add request limit with bucket4j

--- src/main/groovy/rocks/metaldetector/butler/config/AppConfig.groovy
+++ src/main/groovy/rocks/metaldetector/butler/config/AppConfig.groovy
@@ -1,5 +1,6 @@
 package rocks.metaldetector.butler.config
 
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -7,5 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @Configuration
 @EnableJpaAuditing
 @EnableScheduling
+@EnableCaching
 class AppConfig {
 }
