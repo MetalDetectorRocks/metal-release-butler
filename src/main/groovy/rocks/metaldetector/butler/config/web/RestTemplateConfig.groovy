d2f25ae1a50745c07d474b977ecca0090e19b374
Daniel Wagner <wagner.daniel87@gmail.com>
2020-03-17 13:48:15 +0100

    add message converters to resttemplate

--- src/main/groovy/rocks/metaldetector/butler/config/web/RestTemplateConfig.groovy
+++ src/main/groovy/rocks/metaldetector/butler/config/web/RestTemplateConfig.groovy
@@ -1,5 +1,7 @@
 package rocks.metaldetector.butler.config.web
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.impl.client.CloseableHttpClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -7,7 +9,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.http.converter.StringHttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
+
+import static java.nio.charset.StandardCharsets.UTF_8
+import static org.springframework.http.MediaType.TEXT_HTML
+import static org.springframework.http.MediaType.TEXT_PLAIN
 
 @Configuration
 class RestTemplateConfig {
@@ -19,6 +27,31 @@ class RestTemplateConfig {
   RestTemplateConfig(CloseableHttpClient httpClient, @Value('${httpclient.useragent}') String userAgent) {
     this.httpClient = httpClient
     this.userAgent = userAgent
+  }
+
+  @Bean
+  MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+    new MappingJackson2HttpMessageConverter(
+            objectMapper: objectMapper()
+    )
+  }
+
+  @Bean
+  ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper(
+            serializationInclusion: JsonInclude.Include.NON_EMPTY
+    )
+
+    return objectMapper
+  }
+
+  @Bean
+  StringHttpMessageConverter stringHttpMessageConverter() {
+    new StringHttpMessageConverter(
+            defaultCharset: UTF_8,
+            writeAcceptCharset: false,
+            supportedMediaTypes: [TEXT_PLAIN, TEXT_HTML]
+    )
   }
 
   @Bean
@@ -34,6 +67,7 @@ class RestTemplateConfig {
             .requestFactory({ -> clientHttpRequestFactory() })
             .errorHandler(new CustomClientErrorHandler())
             .interceptors(new CustomClientHttpRequestInterceptor(userAgent))
+            .messageConverters([jackson2HttpMessageConverter(), stringHttpMessageConverter()])
             .build()
   }
 }
