83340fa3d90e1ce4e9a82c948a01822cc960fac3
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-04-17 22:27:14 +0200

    Simplified NoAuthSecurityConfig https://trello.com/c/x1MpE505

--- src/main/groovy/rocks/metaldetector/butler/config/security/NoAuthSecurityConfig.groovy
+++ src/main/groovy/rocks/metaldetector/butler/config/security/NoAuthSecurityConfig.groovy
@@ -3,12 +3,10 @@ package rocks.metaldetector.butler.config.security
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import rocks.metaldetector.butler.config.constants.Endpoints
 
-import static org.springframework.http.HttpMethod.GET
-import static org.springframework.http.HttpMethod.POST
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 
 @Configuration
@@ -18,13 +16,9 @@ class NoAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.sessionManagement().sessionCreationPolicy(STATELESS)
+    http.csrf().ignoringAntMatchers(Endpoints.AntPattern.REST_ENDPOINTS)
+        .and().sessionManagement().sessionCreationPolicy(STATELESS)
     http.requiresChannel().requestMatchers(new XForwardedProtoMatcher()).requiresSecure()
-    http.authorizeRequests().antMatchers(GET, "/**").permitAll()
-  }
-
-  @Override
-  void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers(POST, "/**")
+    http.authorizeRequests().anyRequest().permitAll()
   }
 }
