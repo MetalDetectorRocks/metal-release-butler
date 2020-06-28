e6a2ac61bd9d0a711eeec4783358724700c9cb94
Daniel Wagner <wagner.daniel87@gmail.com>
2020-06-28 15:57:41 +0200

    implement and test find all import results

--- src/test/groovy/rocks/metaldetector/butler/DtoFactory.groovy
+++ src/test/groovy/rocks/metaldetector/butler/DtoFactory.groovy
@@ -1,27 +1,55 @@
 package rocks.metaldetector.butler
 
+import rocks.metaldetector.butler.model.importjob.ImportJobEntity
 import rocks.metaldetector.butler.model.release.ReleaseEntity
 import rocks.metaldetector.butler.model.release.ReleaseEntityRecordState
+import rocks.metaldetector.butler.model.release.ReleaseSource
 import rocks.metaldetector.butler.web.dto.ReleaseDto
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 class DtoFactory {
 
   static class ReleaseEntityFactory {
 
     static ReleaseEntity createReleaseEntity(String artist, LocalDate releaseDate) {
-      return new ReleaseEntity(artist: artist, albumTitle: "T", releaseDate: releaseDate, state: ReleaseEntityRecordState.OK,
-                               estimatedReleaseDate: "releaseDate", additionalArtists: artist)
+      return new ReleaseEntity(
+              artist: artist,
+              albumTitle: "T",
+              releaseDate: releaseDate,
+              state: ReleaseEntityRecordState.OK,
+              estimatedReleaseDate: "releaseDate",
+              additionalArtists: artist
+      )
+    }
+  }
+
+  static class ImportJobEntityFactory {
+
+    static ImportJobEntity createImportJobEntity() {
+      return new ImportJobEntity(
+              jobId: UUID.randomUUID(),
+              totalCountRequested: 666,
+              totalCountImported: 666,
+              startTime: LocalDateTime.now(),
+              endTime: LocalDateTime.now(),
+              source: ReleaseSource.METAL_ARCHIVES
+      )
     }
   }
 
   static class ReleaseDtoFactory {
 
     static ReleaseDto createReleaseDto(String artist, LocalDate releaseDate) {
-      return new ReleaseDto(artist: artist, additionalArtists: [artist], albumTitle: "T",
-                            releaseDate: releaseDate, estimatedReleaseDate: "releaseDate",
-                            state: ReleaseEntityRecordState.OK)
+      return new ReleaseDto(
+              artist: artist,
+              additionalArtists: [artist],
+              albumTitle: "T",
+              releaseDate: releaseDate,
+              estimatedReleaseDate: "releaseDate",
+              state: ReleaseEntityRecordState.OK
+      )
     }
   }
 }
