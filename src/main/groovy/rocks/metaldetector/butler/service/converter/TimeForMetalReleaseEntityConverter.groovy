97e9efb853f2da58a97b45b216f9bf3ad3863913
Daniel Wagner <wagner.daniel87@gmail.com>
2020-12-05 17:28:55 +0100

    splitting improvements for 'time for metal release' source

--- src/main/groovy/rocks/metaldetector/butler/service/converter/TimeForMetalReleaseEntityConverter.groovy
+++ src/main/groovy/rocks/metaldetector/butler/service/converter/TimeForMetalReleaseEntityConverter.groovy
@@ -6,17 +6,20 @@ import groovy.xml.slurpersupport.NodeChild
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import rocks.metaldetector.butler.model.release.ReleaseEntity
-import rocks.metaldetector.butler.model.release.ReleaseType
 
 import java.time.LocalDate
 
 import static rocks.metaldetector.butler.model.release.ReleaseEntityState.OK
 import static rocks.metaldetector.butler.model.release.ReleaseSource.TIME_FOR_METAL
-import static rocks.metaldetector.butler.model.release.ReleaseType.*
+import static rocks.metaldetector.butler.model.release.ReleaseType.FULL_LENGTH
 
 @Component
 @Slf4j
 class TimeForMetalReleaseEntityConverter implements Converter<String, List<ReleaseEntity>> {
+
+  static final String ANY_WHITESPACE_REGEX = "\\s*"
+  static final String ANY_DASH_REGEX = "\\p{Pd}"
+  static final String ARTIST_ALBUM_NAME_DELIMITER_REGEX = "${ANY_WHITESPACE_REGEX}${ANY_DASH_REGEX}${ANY_WHITESPACE_REGEX}"
 
   @Autowired
   XmlSlurper xmlSlurper
@@ -45,20 +48,21 @@ class TimeForMetalReleaseEntityConverter implements Converter<String, List<Relea
     return releases as List<ReleaseEntity>
   }
 
-  private void setArtistName(def builder, String string) {
-    builder.artist(string.split(" - ")[0])
+  private void setArtistName(def builder, String rawValue) {
+    String artistName = rawValue.split(ARTIST_ALBUM_NAME_DELIMITER_REGEX)[0]
+    builder.artist(artistName.trim().strip())
   }
 
-  private void setAlbumTitle(def builder, String string) {
+  private void setAlbumTitle(def builder, String rawValue) {
     def albumTitle = ""
-    string.split(" - ").eachWithIndex { it, index ->
+    rawValue.split(ARTIST_ALBUM_NAME_DELIMITER_REGEX).eachWithIndex { it, index ->
       if (index == 1) {
         albumTitle = it
       } else {
         albumTitle += " - ${it}"
       }
     }
-    builder.albumTitle(albumTitle)
+    builder.albumTitle(albumTitle.trim().strip())
   }
 
   private void setReleaseDate(def builder, String dateString) {
@@ -68,6 +72,7 @@ class TimeForMetalReleaseEntityConverter implements Converter<String, List<Relea
 
   private void setCoverSourceUrl(def builder, NodeChild rawSource) {
     def sourceUrl = rawSource.attributes()["src"] as String
+    sourceUrl.replaceAll(ANY_DASH_REGEX, "-")
     builder.releaseDetailsUrl(sourceUrl)
   }
 }
