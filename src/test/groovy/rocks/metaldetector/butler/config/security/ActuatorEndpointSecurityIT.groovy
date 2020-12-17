445261b00cba0467838f10cc13d20b354ea5a382
Daniel Wagner <wagner.daniel87@gmail.com>
2020-12-17 10:47:51 +0100

    do not secure actuator endpoints

---
+++ src/test/groovy/rocks/metaldetector/butler/config/security/ActuatorEndpointSecurityIT.groovy
@@ -0,0 +1,40 @@
+package rocks.metaldetector.butler.config.security
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.servlet.MockMvc
+import rocks.metaldetector.butler.testutil.WithIntegrationTestConfig
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ActuatorEndpointSecurityIT extends Specification implements WithIntegrationTestConfig {
+
+  @Autowired
+  MockMvc mockMvc
+
+  @Unroll
+  "actuator endpoint #endpoint is not secured"() {
+    given:
+    def request = get(endpoint)
+
+    when:
+    def result = mockMvc.perform(request).andReturn()
+
+    then:
+    result.response.status == HttpStatus.OK.value()
+
+    where:
+    endpoint << [
+            "/actuator",
+            "/actuator/health",
+            "/actuator/info",
+            "/actuator/metrics"
+    ]
+  }
+}
