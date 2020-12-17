a7a97b4bba684c7e1255ca7d7992019b62cff731
Daniel Wagner <wagner.daniel87@gmail.com>
2020-12-17 15:00:23 +0100

    add more needed docker secrets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,11 +21,13 @@ file_env() {
 }
 
 envs=(
+  DATASOURCE_URL
   DATASOURCE_USERNAME
   DATASOURCE_PASSWORD
   AWS_ACCESS_KEY
   AWS_SECRET_KEY
   JWT_SECRET
+  ACTUATOR_BASE_PATH
 )
 
 for e in "${envs[@]}"; do
