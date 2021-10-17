bea6d7273f43b1dd257f08084a2e8e33b0771775
hedgehog1833 <nilsdam@aol.com>
2021-10-17 10:51:09 +0200

    HttpBuilder removed and dependency updates

--- supplier/metal-archives/src/main/groovy/rocks/metaldetector/butler/supplier/metalarchives/cover/MetalArchivesCoverFetcher.groovy
+++ supplier/metal-archives/src/main/groovy/rocks/metaldetector/butler/supplier/metalarchives/cover/MetalArchivesCoverFetcher.groovy
@@ -1,7 +1,7 @@
 package rocks.metaldetector.butler.supplier.metalarchives.cover
 
 import groovy.util.logging.Slf4j
-import groovyx.net.http.HttpBuilder
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
@@ -10,7 +10,6 @@ import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import rocks.metaldetector.butler.supplier.infrastructure.ReleaseEntityDeleteRequestEvent
 import rocks.metaldetector.butler.supplier.infrastructure.cover.CoverFetcher
-import rocks.metaldetector.butler.supplier.infrastructure.cover.HttpBuilderFunction
 
 import java.util.concurrent.TimeUnit
 
@@ -24,16 +23,12 @@ class MetalArchivesCoverFetcher implements CoverFetcher {
   int currentAttempt
 
   @Autowired
-  HttpBuilderFunction httpBuilderFunction
-
-  @Autowired
   ApplicationEventPublisher eventPublisher
 
   @Override
   String fetchCoverUrl(String sourceUrl) {
     currentAttempt = 0
-    HttpBuilder httpBuilder = httpBuilderFunction.apply(sourceUrl)
-    Document releasePage = fetchReleasePage(httpBuilder, sourceUrl)
+    Document releasePage = fetchReleasePage(sourceUrl)
     Elements coverDivs = releasePage?.select("a")
         ?.findAll { it.id() == ALBUM_COVER_HTML_ID }
     Element coverDiv = coverDivs ? coverDivs.first() : null
@@ -41,17 +36,17 @@ class MetalArchivesCoverFetcher implements CoverFetcher {
     return coverLink
   }
 
-  private Document fetchReleasePage(HttpBuilder httpBuilder, String sourceUrl) {
+  private Document fetchReleasePage(String sourceUrl) {
     try {
       currentAttempt++
-      return httpBuilder.get() as Document
+      return Jsoup.connect(sourceUrl).get()
     }
     catch (Exception e) {
-      return handleErrorAndTryAgain(e, httpBuilder, sourceUrl)
+      return handleErrorAndTryAgain(e, sourceUrl)
     }
   }
 
-  private Document handleErrorAndTryAgain(Exception exception, HttpBuilder httpBuilder, String sourceUrl) {
+  private Document handleErrorAndTryAgain(Exception exception, String sourceUrl) {
     if (exception.message?.containsIgnoreCase(RELEASE_PAGE_NOT_FOUND_ERROR_MESSAGE)) {
       log.info("The release page '${sourceUrl}' could not be found.")
       eventPublisher.publishEvent(new ReleaseEntityDeleteRequestEvent(this, sourceUrl))
@@ -61,7 +56,7 @@ class MetalArchivesCoverFetcher implements CoverFetcher {
       log.info("The release page '${sourceUrl}' could not be fetched. Reason: $exception.message. " +
                "I will wait 1 second and try again ($currentAttempt/$MAX_ATTEMPTS).")
       TimeUnit.SECONDS.sleep(1)
-      return fetchReleasePage(httpBuilder, sourceUrl)
+      return fetchReleasePage(sourceUrl)
     }
     else {
       log.error("5 errors in a row during fetching the release page. I give up.", exception)
