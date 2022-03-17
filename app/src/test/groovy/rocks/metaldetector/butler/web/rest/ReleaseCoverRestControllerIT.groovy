f123c3e3f032b3abfb7c387c74bf9ff852081d63
Nils Dammenhayn <nilsdam@aol.com>
2022-03-17 19:57:57 +0100

    IntegrationTest repaired

--- app/src/test/groovy/rocks/metaldetector/butler/web/rest/ReleaseCoverRestControllerIT.groovy
+++ app/src/test/groovy/rocks/metaldetector/butler/web/rest/ReleaseCoverRestControllerIT.groovy
@@ -8,9 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.test.web.servlet.MockMvc
+import rocks.metaldetector.butler.config.web.ResourceNotFoundException
 import rocks.metaldetector.butler.service.release.ImageResourceFinder
 import rocks.metaldetector.butler.testutil.WithIntegrationTestConfig
 import spock.lang.Specification
+
+import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static rocks.metaldetector.butler.supplier.infrastructure.Endpoints.RELEASE_IMAGES
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,18 +35,17 @@ class ReleaseCoverRestControllerIT extends Specification implements WithIntegrat
   @SpringBean
   ImageResourceFinder imageResourceFinder = Mock(ImageResourceFinder)
 
-//  def "should return NOT FOUND for wrong path"() {
-//    given:
-//    jwtDecoder.decode(*_) >> RELEASES_READ_JWT
-//    def request = get("${RELEASE_IMAGES}/?id=test-id")
-//        .accept(IMAGE_JPEG_VALUE, IMAGE_GIF_VALUE, IMAGE_PNG_VALUE)
-//        .header("Authorization", "Bearer $RELEASES_READ_JWT.tokenValue")
-//    imageResourceFinder.findImage(*_) >> { throw new ResourceNotFoundException("not found") }
-//
-//    when:
-//    def result = mockMvc.perform(request).andReturn()
-//
-//    then:
-//    result.response.status == NOT_FOUND.value()
-//  }
+  def "should return NOT FOUND for wrong path"() {
+    given:
+    jwtDecoder.decode(*_) >> RELEASES_READ_JWT
+    def request = get("${RELEASE_IMAGES}/?id=test-id")
+        .header("Authorization", "Bearer $RELEASES_READ_JWT.tokenValue")
+    imageResourceFinder.findImage(*_) >> { throw new ResourceNotFoundException("not found") }
+
+    when:
+    def result = mockMvc.perform(request).andReturn()
+
+    then:
+    result.response.status == NOT_FOUND.value()
+  }
 }
