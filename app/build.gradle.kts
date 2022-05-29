1ff0558930d4fcdfaaffecbc322fe9aa294c670d
Nils Dammenhayn <nilsdam@aol.com>
2022-05-29 13:55:51 +0200

    gradle foo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ dockerPublish {
   organisation.set("metaldetector")
   imageName.set(rootProject.name)
 }
+
 springBoot {
   mainClass.set("rocks.metaldetector.butler.MetalReleaseButlerApplication")
   buildInfo().apply {
@@ -55,9 +56,7 @@ dependencies {
   developmentOnly("org.springframework.boot:spring-boot-devtools:${libs.versions.springBoot.get()}")
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:${libs.versions.springBoot.get()}")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-test:${libs.versions.springBoot.get()}") {
-    exclude(group = "junit", module = "junit")
-  }
+  testImplementation("org.springframework.boot:spring-boot-starter-test:${libs.versions.springBoot.get()}")
   testImplementation("org.apache.groovy:groovy-test:${libs.versions.groovy.get()}")
   testImplementation("org.spockframework:spock-core:${libs.versions.spock.get()}")
   testImplementation("org.spockframework:spock-spring:${libs.versions.spock.get()}")
