5e20b8fc18b88a524d66303aa23c7b58ecb048cc
Nils Dammenhayn <nilsdam@aol.com>
2022-08-13 15:19:36 +0200

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,7 @@ val dependencyVersions = listOf(
     "commons-logging:commons-logging:1.2",
     "commons-codec:commons-codec:1.15",
     "org.ow2.asm:asm:9.3",
-    "net.bytebuddy:byte-buddy:1.12.13",
-    "org.assertj:assertj-core:3.23.1",
     "org.slf4j:slf4j-api:1.7.36",
-    "org.apiguardian:apiguardian-api:1.1.2",
     "org.jboss.logging:jboss-logging:3.5.0.Final",
     "org.junit:junit-bom:${libs.versions.junit.get()}"
 )
