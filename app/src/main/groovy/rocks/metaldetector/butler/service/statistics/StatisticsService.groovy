a87f21752c7ab9a96a07eace4851259bcec8eee1
Nils Dammenhayn <nilsdam@aol.com>
2023-09-13 22:21:35 +0200

    import info added to statistics endpoint

--- app/src/main/groovy/rocks/metaldetector/butler/service/statistics/StatisticsService.groovy
+++ app/src/main/groovy/rocks/metaldetector/butler/service/statistics/StatisticsService.groovy
@@ -3,20 +3,27 @@ package rocks.metaldetector.butler.service.statistics
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import rocks.metaldetector.butler.persistence.domain.importjob.ImportJobRepository
 import rocks.metaldetector.butler.persistence.domain.release.ReleaseRepository
+import rocks.metaldetector.butler.persistence.domain.release.ReleaseSource
+import rocks.metaldetector.butler.web.api.ImportInfo
 import rocks.metaldetector.butler.web.api.ReleaseInfo
 
 import java.time.LocalDate
 import java.time.YearMonth
 
+import static rocks.metaldetector.butler.persistence.domain.importjob.JobState.SUCCESSFUL
 import static rocks.metaldetector.butler.persistence.domain.release.ReleaseEntityState.DEMO
 import static rocks.metaldetector.butler.persistence.domain.release.ReleaseEntityState.DUPLICATE
+import static rocks.metaldetector.butler.persistence.domain.release.ReleaseSource.TEST
 
 @Service
 class StatisticsService {
 
   @Autowired
   ReleaseRepository releaseRepository
+  @Autowired
+  ImportJobRepository importJobRepository
 
   @Transactional(readOnly = true)
   ReleaseInfo getReleaseInfo() {
@@ -31,5 +38,20 @@ class StatisticsService {
                            upcomingReleases: upcomingReleases,
                            releasesThisMonth: releasesPerMonth[YearMonth.now()] ?: 0,
                            duplicates: duplicates)
+  }
+
+  @Transactional(readOnly = true)
+  List<ImportInfo> getImportInfo() {
+    return (ReleaseSource.values() - TEST).collect { source ->
+      long allImports = importJobRepository.countBySource(source)
+      long successFullImports = importJobRepository.countBySourceAndState(source, SUCCESSFUL)
+      int successRate = (successFullImports / allImports * 100).intValue()
+      def lastImport = importJobRepository.findLastStartTime(source)
+      def lastSuccessfulImport = importJobRepository.findLastStartTimeByState(source, SUCCESSFUL)
+      return new ImportInfo(source: source.name(),
+                            successRate: successRate,
+                            lastImport: lastImport,
+                            lastSuccessfulImport: lastSuccessfulImport)
+    }
   }
 }
--- persistence/src/main/groovy/rocks/metaldetector/butler/persistence/domain/importjob/ImportJobRepository.groovy
+++ persistence/src/main/groovy/rocks/metaldetector/butler/persistence/domain/importjob/ImportJobRepository.groovy
@@ -1,9 +1,23 @@
 package rocks.metaldetector.butler.persistence.domain.importjob
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import rocks.metaldetector.butler.persistence.domain.release.ReleaseSource
+
+import java.time.LocalDateTime
 
 @Repository
 interface ImportJobRepository extends JpaRepository<ImportJobEntity, Long> {
 
+  long countBySource(ReleaseSource source)
+
+  long countBySourceAndState(ReleaseSource source, JobState state)
+
+  @Query("select i.startTime from import_jobs i where i.source = :source order by i.startTime desc limit 1")
+  LocalDateTime findLastStartTime(@Param("source") ReleaseSource source)
+
+  @Query("select i.startTime from import_jobs i where i.source = :source and i.state = :state order by i.startTime desc limit 1")
+  LocalDateTime findLastStartTimeByState(@Param("source") ReleaseSource source, @Param("state") JobState state)
 }
