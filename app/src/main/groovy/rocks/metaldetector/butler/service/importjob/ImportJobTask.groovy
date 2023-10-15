1b6a6658b9fa7dc29649620a59a8edefe71a3828
Nils Dammenhayn <nilsdam@aol.com>
2023-10-15 16:15:49 +0200

    review: update importjob in try to avoid later null-checking

--- app/src/main/groovy/rocks/metaldetector/butler/service/importjob/ImportJobTask.groovy
+++ app/src/main/groovy/rocks/metaldetector/butler/service/importjob/ImportJobTask.groovy
@@ -19,16 +19,13 @@ class ImportJobTask implements Runnable {
   @Override
   void run() {
     importJobService.updateImportJob(importJob, RUNNING)
-    ImportResult importResult = null
     try {
-      importResult = releaseImporter.importReleases()
+      ImportResult importResult = releaseImporter.importReleases()
+      importJobService.updateImportJob(importJob, SUCCESSFUL, importResult)
     }
     catch (Exception e) {
       log.error("Error during import of releases from '${releaseImporter.getReleaseSource()?.displayName}'", e)
       importJobService.updateImportJob(importJob, ERROR)
-    }
-    if (importResult) {
-      importJobService.updateImportJob(importJob, SUCCESSFUL, importResult)
     }
   }
 }
